72eb55e32d928431ab7b5d98f8c29eba998a6b0d
vinhnphan <79555615+vinhnphan@users.noreply.github.com>
2021-06-21 18:16:46 +0700

    feat-project-init

---
+++ src/Home/Content0.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import QueueAnim from 'rc-queue-anim';
+import { Row, Col } from 'antd';
+import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
+import TweenOne from 'rc-tween-one';
+import { getChildrenToRender } from './utils';
+
+const loop = {
+  duration: 3000,
+  yoyo: true,
+  repeat: -1,
+};
+
+class Content extends React.PureComponent {
+  render() {
+    const { dataSource, isMobile, ...props } = this.props;
+    const {
+      wrapper,
+      titleWrapper,
+      page,
+      OverPack: overPackData,
+      childWrapper,
+    } = dataSource;
+    return (
+      <div style={{ position: 'relative' }} {...props} {...wrapper}>
+        <div style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }} className="banner-bg-wrapper">
+          <svg width="100%" height="100%" viewBox="0 0 400 576" fill="none">
+            <g transform="translate(50, 100)">
+              <rect id="Rectangle-33" stroke="none" fill="#EBEDF0" fillRule="evenodd" transform="translate(1261.132034, 1217.132034) rotate(45.000000) translate(-1261.132034, -1217.132034) " x="1111.13203" y="1007.13203" width="300" height="300" rx="1" />
+              <circle id="Oval-13" stroke="#EBEDF0" strokeWidth="16" fill="none" cx="1402" cy="151" r="70" />
+              <path d="M385.032144,960.394832 L394.316344,976.475539 C394.868629,977.432124 394.540879,978.655305 393.584293,979.20759 C393.280255,979.383126 392.935367,979.475539 392.584293,979.475539 L374.015893,979.475539 C372.911323,979.475539 372.015893,978.580108 372.015893,977.475539 C372.015893,977.124466 372.108305,976.779577 372.283842,976.475539 L381.568042,960.394832 C382.120327,959.438247 383.343508,959.110497 384.300093,959.662781 C384.604131,959.838318 384.856607,960.090794 385.032144,960.394832 Z" id="Polygon-2" stroke="none" fill="#A3B1BF" fillRule="evenodd" transform="translate(383.300093, 970.709623) rotate(70.000000) translate(-383.300093, -970.709623) " />
+              <path d="M545.537489,211.431472 L552.545207,223.569196 C553.097492,224.525781 552.769741,225.748962 551.813156,226.301246 C551.509118,226.476783 551.164229,226.569196 550.813156,226.569196 L536.79772,226.569196 C535.693151,226.569196 534.79772,225.673765 534.79772,224.569196 C534.79772,224.218122 534.890133,223.873234 535.06567,223.569196 L542.073387,211.431472 C542.625672,210.474887 543.848853,210.147137 544.805438,210.699421 C545.109477,210.874958 545.361952,211.127434 545.537489,211.431472 Z" id="Polygon-2" stroke="none" fill="#A3B1BF" fillRule="evenodd" transform="translate(543.805605, 218.500167) rotate(90.000000) translate(-543.805605, -218.500167) " />
+            </g>
+
+            <g id="Group-29" transform="translate(10.000000, 100.000000)" fill="#2F54EB">
+              <image xlinkHref="https://gw.alipayobjects.com/zos/rmsportal/VrADJaRPMnFjmtmIhObV.svg" />
+            </g>
+            <circle id="Oval-8" stroke="#13C2C2" strokeWidth="4" opacity="0.95" cx="1096" cy="11" r="11" />
+            <circle id="Oval-8" stroke="#13C2C2" strokeWidth="4" cx="11" cy="667" r="11" />
+            <g id="Group-11" transform="translate(600.000000, 419.000000)" fill="#13C2C2">
+              <image xlinkHref="https://gw.alipayobjects.com/zos/rmsportal/MnLEmwjipfhzPUmBJnJE.svg" />
+            </g>
+          </svg>
+        </div>
+        <div {...page}>
+          <div {...titleWrapper}>
+            {titleWrapper.children.map(getChildrenToRender)}
+          </div>
+          <OverPack {...overPackData}>
+            <QueueAnim
+              type="bottom"
+              key="block"
+              leaveReverse
+              component={Row}
+              componentProps={childWrapper}
+            >
+              {childWrapper.children.map((block, i) => {
+                const { children: item, ...blockProps } = block;
+                return (
+                  <Col key={i.toString()} {...blockProps}>
+                    <div {...item}>
+                      {item.children.map(getChildrenToRender)}
+                    </div>
+                  </Col>
+                );
+              })}
+            </QueueAnim>
+          </OverPack>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Content;
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,53 +1,174 @@
+/* eslint no-undef: 0 */
+/* eslint arrow-parens: 0 */
 import React from 'react';
-import { addLocaleData, IntlProvider } from 'react-intl';
-import DocumentTitle from 'react-document-title';
 import { enquireScreen } from 'enquire-js';
-import Header from './Header';
-import Banner from './Banner';
-import Page1 from './Page1';
-import Page2 from './Page2';
-import Page3 from './Page3';
-import Footer from './Footer';
-import cnLocale from '../zh-CN';
-import './static/style';
 
-let isMobile = false;
+import Nav3 from './Nav3';
+import Banner5 from './Banner5';
+import Content0 from './Content0';
+import Feature8 from './Feature8';
+import Feature6 from './Feature6';
+import Content1 from './Content1';
+import Feature2 from './Feature2';
+import Feature1 from './Feature1';
+import Content9 from './Content9';
+import Teams3 from './Teams3';
+import Content4 from './Content4';
+import Content11 from './Content11';
+import Point from './Point';
+import {
+  Nav30DataSource,
+  Banner50DataSource,
+  Content00DataSource,
+  Feature80DataSource,
+  Feature60DataSource,
+  Content10DataSource,
+  Feature20DataSource,
+  Feature10DataSource,
+  Content90DataSource,
+  Teams30DataSource,
+  Content40DataSource,
+  Content110DataSource,
+} from './data.source';
+import './less/antMotionStyle.less';
+
+let isMobile;
 enquireScreen((b) => {
   isMobile = b;
 });
 
-class Home extends React.PureComponent {
+const { location = {} } = typeof window !== 'undefined' ? window : {};
+
+export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    const appLocale = cnLocale;
-    addLocaleData(appLocale.data);
     this.state = {
-      appLocale,
       isMobile,
+      show: !location.port, // 如果不是 dva 2.0 请删除
     };
   }
+
   componentDidMount() {
+    // 适配手机屏幕;
     enquireScreen((b) => {
-      this.setState({
-        isMobile: !!b,
-      });
+      this.setState({ isMobile: !!b });
     });
+    // dva 2.0 样式在组件渲染之后动态加载，导致滚动组件不生效；线上不影响；
+    /* 如果不是 dva 2.0 请删除 start */
+    if (location.port) {
+      // 样式 build 时间在 200-300ms 之间;
+      setTimeout(() => {
+        this.setState({
+          show: true,
+        });
+      }, 500);
+    }
+    /* 如果不是 dva 2.0 请删除 end */
   }
+
   render() {
-    const { appLocale } = this.state;
+    const children = [
+      <Nav3
+        id="Nav3_0"
+        key="Nav3_0"
+        dataSource={Nav30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Banner5
+        id="Banner5_0"
+        key="Banner5_0"
+        dataSource={Banner50DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content0
+        id="Content0_0"
+        key="Content0_0"
+        dataSource={Content00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature8
+        id="Feature8_0"
+        key="Feature8_0"
+        dataSource={Feature80DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature6
+        id="Feature6_0"
+        key="Feature6_0"
+        dataSource={Feature60DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content1
+        id="Content1_0"
+        key="Content1_0"
+        dataSource={Content10DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature2
+        id="Feature2_0"
+        key="Feature2_0"
+        dataSource={Feature20DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature1
+        id="Feature1_0"
+        key="Feature1_0"
+        dataSource={Feature10DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content9
+        id="Content9_0"
+        key="Content9_0"
+        dataSource={Content90DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Teams3
+        id="Teams3_0"
+        key="Teams3_0"
+        dataSource={Teams30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content4
+        id="Content4_0"
+        key="Content4_0"
+        dataSource={Content40DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Content11
+        id="Content11_0"
+        key="Content11_0"
+        dataSource={Content110DataSource}
+        isMobile={this.state.isMobile}
+      />, // 导航和页尾不进入锚点区，如果需要，自行添加;
+      <Point
+        key="list"
+        data={[
+          'Nav3_0',
+          'Banner5_0',
+          'Content0_0',
+          'Feature8_0',
+          'Feature6_0',
+          'Content1_0',
+          'Feature2_0',
+          'Feature1_0',
+          'Content9_0',
+          'Teams3_0',
+          'Content4_0',
+          'Content11_0',
+        ]}
+      />,
+    ];
     return (
-      <IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
-        <div className="page-wrapper home">
-          <Header />
-          <Banner isMobile={this.state.isMobile} />
-          <Page1 isMobile={this.state.isMobile} />
-          <Page2 isMobile={this.state.isMobile} />
-          <Page3 />
-          <Footer />
-          <DocumentTitle title="Ant-Design" key="title" />
-        </div>
-      </IntlProvider>
+      <div
+        className="templates-wrapper"
+        ref={(d) => {
+          this.dom = d;
+        }}
+      >
+        {/* 如果不是 dva 2.0 替换成 {children} start */}
+        {this.state.show && children}
+        {/* 如果不是 dva 2.0 替换成 {children} end */}
+      </div>
     );
   }
 }
-export default Home;
